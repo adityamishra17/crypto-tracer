50dd2ea1bd71d7ab719c99539bd4e94a7626dc5d
Aditya Mishra <adityamishra1711@gmail.com>
2024-06-01 21:46:47 +0530

    fsetup

---
+++ cc-tracer/src/config/FirebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBx8guKQP54hpfJ5_WP5g94GnUT3_DllI0",
+    authDomain: "cc-tracer-1fec6.firebaseapp.com",
+    projectId: "cc-tracer-1fec6",
+    storageBucket: "cc-tracer-1fec6.appspot.com",
+    messagingSenderId: "42482834546",
+    appId: "1:42482834546:web:94d0c3f5671e53865e36eb"
+  };
+
+  export default firebaseConfig;
